ba43a48b88c59271aa56c29276e201c0f1f5b5da
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2025-09-13 17:14:03 -0300

    ordenando

--- 1-dataset-processing/main.py
+++ 1-dataset-processing/main.py
@@ -1,11 +1,5 @@
 import os
-import glob
-import numpy as np
-import matplotlib.pyplot as plt
-import cv2
 import argparse
-
-from tqdm import tqdm
 
 from utils.image_processing import (
     np_zero_image,
@@ -27,19 +21,20 @@ CLASSES = ["stone", "normal"]
 
 if __name__ == "__main__":
     
-    parser = argparse.ArgumentParser(
-        description="Image patches processing"
-    )
+    parser = argparse.ArgumentParser(description="Image patches processing")
     parser.add_argument("-ps", "--patch_size", required=True, type=int)
     args = parser.parse_args()
+    
     patch_size = args.patch_size
     
-    generate_images_patches(
-        class_name_list=CLASSES, img_pacth_size=patch_size, ds_path=ds_path
-    )
+    generate_images_patches(class_name_list=CLASSES, 
+                            img_pacth_size=patch_size, 
+                            ds_path=ds_path )
     
     out_path = f"{out_path}dataset-{patch_size}"
     
     if os.path.exists(out_path):
         X, y = datasets_gen(out_path=out_path)   
         datasets_gen_kfold(X, y, num_folds=5, out_path=out_path)
+
+
