bbdbd0bf9c1977040130b86512b2cee3bcef98c9
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2025-09-16 21:51:54 -0300

    Update main.py

--- 2-training-kfold/main.py
+++ 2-training-kfold/main.py
@@ -9,7 +9,7 @@ def train_fold(input_path: str,output_path: str, train_filename: str = "train0.c
 
 if __name__ == "__main__":
   input_path = "../1-dataset-processing/data/dataset-article/dataset-224"
-  output_path = "output/dataset-224"
+  output_path = "output/dataset-224/train0"
   train_filename = "train0.csv" 
   val_filename = "val0.csv"  
   train_fold(input_path,output_path, train_filename = "train0.csv", val_filename = "val0.csv")
